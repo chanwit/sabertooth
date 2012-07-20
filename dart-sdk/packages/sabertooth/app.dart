f972200ba052e9766f63c12c39d9927c54d6a3f0
Chanwit Kaewkasi <chanwit@gmail.com>
2012-07-20 22:59:12 +0700

    improve DSL

---
+++ dart-sdk/packages/sabertooth/app.dart
@@ -0,0 +1,11 @@
+#library('sabertooth:builder');
+
+#import('package:rikulo/view/view.dart');
+#import('package:rikulo/app/app.dart', prefix:"app");
+
+#import('builder.dart');
+
+class Activity extends app.Activity {
+
+
+}
--- dart-sdk/packages/sabertooth/builder.dart
+++ dart-sdk/packages/sabertooth/builder.dart
@@ -2,18 +2,64 @@
 
 #import('package:rikulo/view/view.dart');
 
-TextView textView([String text]) => new TextView(text);
+q(View v) {
+  new Builder(v);
+}
 
-q(View view) => new Builder(view);
-
-String TYPE = "type";
-
-View hlayout(Map<String, String> profile, Map<String, String> layout) {
+Builder hlayout([Map<String, Dynamic> profile, Map<String, Dynamic> layout]) {
   View self = new View();
-  self.layout.type = layout['type'];
+  self.layout.type  = layout['type'];
+  self.layout.align = layout['align'];
   self.profile.width  = profile['width'];
   self.profile.height = profile['height'];
-  return self;
+  return new Builder(self);
+}
+
+Builder view([String id, Map<String, Dynamic> style, Map<String, Dynamic> profile, Map<String, Dynamic> layout]) {
+  View self = new View();
+  if(id != null) {
+    self.id = id;
+  }
+  if(style != null) {
+    self.style.backgroundColor = style['backgroundColor'];
+  }
+  if(layout != null) {
+      self.layout.type = layout['type'];
+  }
+  if(profile != null) {
+    self.profile.width  = profile['width'];
+    self.profile.height = profile['height'];
+  }
+  return new Builder(self);
+}
+
+Builder text(String value, [String id, Map<String, Dynamic> style, Map<String, Dynamic> profile]) {
+  TextView self = new TextView(value);
+  if(id != null) {
+    self.id = id;
+  }
+  if(style != null) {
+    self.style.border = style['border'];
+  }
+  if(profile != null) {
+    self.profile.anchor     = profile['anchor'];
+    self.profile.anchorView = profile['anchorView'];
+    print("anchorView: ${profile['anchorView']}");
+    self.profile.location   = profile['location'];
+  }
+  return new Builder(self);
+}
+
+class Color {
+  static final String red    = "red";
+  static final String blue   = "blue";
+  static final String orange = "orange";
+  static final String yellow = "yellow";
+}
+
+class Profile {
+  static final String content = "content";
+  static final String flex    = "flex";
 }
 
 class Builder {
@@ -35,16 +81,18 @@ class Builder {
     return this;
   }
 
-  Builder profile([String width, String height]) {
+  Builder profile([String width, String height, View anchorView]) {
     _view.profile.width  = width;
     _view.profile.height = height;
+    _view.profile.anchorView = anchorView;
     return this;
   }
 
-  Builder operator << (List<View> views) {
-    for(View v in views) {
-      _view.addChild(v);
+  Builder operator << (List<Builder> builders) {
+    for(Builder b in builders) {
+      _view.addChild(b._view);
     }
+    return this;
   }
 
 }
